345b2def643d86261243de139cab28a68fcdb78f
Atsuo MURATA <rikuma95@gmail.com>
2021-01-01 20:03:39 +0900

    first commit

---
+++ Remove_YouTube_recommendation/script.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Remove YouTube recommendation
+// @version      0.1
+// @namespace    https://github.com/amurata/userscripts
+// @description  Remove the last YouTube recommendation window
+// @author       Atsuo MURATA
+// @include      https://www.youtube.com/*
+// ==/UserScript==
+
+window.onload = function () {
+  'use strict';
+    const recommendElem = '.ytp-ce-element'
+    const removes = document.querySelectorAll(recommendElem);
+    for (let i = 0; i < removes.length; i++) {
+        removes[i].style.display = 'none';
+    }
+}
