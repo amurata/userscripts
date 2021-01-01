5de3a15d98687465d7bffb10b9420bccc496f51d
amurata <rikuma95@gmail.com>
2021-01-01 20:28:38 +0900

    Update script.user.js

--- Remove_YouTube_recommendation/script.user.js
+++ Remove_YouTube_recommendation/script.user.js
@@ -7,11 +7,11 @@
 // @include      https://www.youtube.com/*
 // ==/UserScript==
 
-window.onload = function () {
+(window.onload = function () {
   'use strict';
     const recommendElem = '.ytp-ce-element'
     const removes = document.querySelectorAll(recommendElem);
     for (let i = 0; i < removes.length; i++) {
         removes[i].style.display = 'none';
     }
-}
+})();
